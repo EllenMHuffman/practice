46de801b5e56b0740ffda30ea119d88e2dd99102
Ellen Huffman <EllenMHuffman@gmail.com>
2018-04-04 12:40:47 -0700

    add find anagrams algorithm

---
+++ anagrams.py
@@ -0,0 +1,29 @@
+# Given list of words, return a list of lists where each list contains words
+# that are anagrams of each other.
+from collections import defaultdict
+
+
+def find_anagrams(words):
+    """Returns lists of words that are anagrams of each other.
+
+    >>> find_anagrams(['bus', 'sub', 'pythonic', 'hypnotic'])
+    >>> [['bus' 'sub'], ['pythonic', 'hypnotic']]
+
+    """
+
+    anagram_groups = []
+
+    anagrams = defaultdict(list)
+
+    for word in words:
+        key = ''.join(sorted(word))
+        anagrams[key].append(word)
+
+    for anagrams in anagrams.values():
+        if len(anagrams) > 1:
+            anagram_groups.append(anagrams)
+
+    return anagram_groups
+
+
+find_anagrams(['bus', 'sub', 'pythonic', 'hypnotic'])
