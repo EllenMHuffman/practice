f3da007d883f02990273f97509d571efc0775f00
Ellen Huffman <EllenMHuffman@gmail.com>
2018-04-03 19:15:36 -0700

    include algorithm to find k largest elements in list

---
+++ n_largest_items.py
@@ -0,0 +1,25 @@
+# Given a list of ~1,000,000 numbers in random order, find the largest 10
+# elements of the list.
+
+
+def find_k_largest(k, numbers):
+    """Find the k largest elements in given list. O(n) runtime."""
+
+    largest = numbers[0]
+
+    for num in numbers:
+        if num > largest:
+            largest = num
+
+    items = [None] * (largest + 1)
+
+    for num in numbers:
+        items[num] = num
+
+    largest_items = []
+
+    for item in reversed(items):
+        if item is not None:
+            largest_items.append(item)
+        if len(largest_items) == k:
+            return largest_items
