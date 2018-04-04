bf25571f586c1470c87450ce4398cf5c98c6a83e
Ellen Huffman <EllenMHuffman@gmail.com>
2018-04-04 13:54:58 -0700

    include function to return sets of 3 elements that sum to k

--- pair_sum.py
+++ pair_sum.py
@@ -19,3 +19,23 @@ def has_sum(nums, k):
                 return True
 
     return False
+
+
+def find_triple_sums(nums, k):
+    """Returns all sets of 3 elements that sum to k.
+
+    >>> find_triple_sums([1,2,3], 6)
+    [[1, 2, 3]]
+    >>> find_triple_sums([1,2,3,6,7], 10)
+    [[1, 2, 7], [1, 3, 6]]
+    """
+
+    triple_sets = []
+
+    for i, num in enumerate(nums[:-2]):
+        for j, alt in enumerate(nums[i + 1:-1]):
+            for third in nums[j + 1:]:
+                if num + alt + third == k:
+                    triple_sets.append([num, alt, third])
+
+    return triple_sets
